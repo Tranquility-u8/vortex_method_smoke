7f537c3c89819a75f193a29eac52ecacd15176ac
Tranquility <2873561640@qq.com>
2024-06-06 14:54:22 +0800

    Add heat field

--- Assets/Scripts/ParticleSystem.cs
+++ Assets/Scripts/ParticleSystem.cs
@@ -3,37 +3,40 @@ using UnityEngine;
 
 namespace VortexMethod 
 {
-
+    [HelpURL("https://zhuanlan.zhihu.com/p/425766028")] 
     public class ParticleSystem : MonoBehaviour
     {
         /// <summary>
         /// 
-        /// 目前是基于面向对象的实现
-        /// 纯CPU，所以性能非常差orz
-        /// 
         /// TODO LISTS：
         /// 1）GPU加速
-        /// 2) 渲染实现
-        /// 3）边界值处理
-        /// 4）与场景交互
-        /// 5）Treecode性能优化
-        /// 
-        /// PS：
-        /// 1到4感觉应该不难
-        /// 5的话看情况实现？
+        /// 2）边界值处理
+        /// 3）与场景交互
+        /// 4）Treecode优化
         /// 
         /// </summary>
         /// 
-        [Header("Parameters")]
+        [Header("Particle")]
         [SerializeField] [Range(0f, 1f)] private float Dt = 0.1f; //时间步长
         [SerializeField] [Range(0f, 0.1f)] private float EPS; //松弛因子
+        
         private int NUM_VORTEX;
         [SerializeField] private int NUM_TRACER;
         [SerializeField] private List<ParticleConfig> vortex_particle_configs;
+        
+        [Header("Heat Field")]
+        [SerializeField] private float HEAT_RADIUS;
+        [SerializeField] [Range(1, 10)] private float HEAT_X_SCALE;
+        [SerializeField] [Range(1, 10)] private float HEAT_Y_SCALE;
+        [SerializeField] [Range(1, 10)] private float HEAT_Z_SCALE;
+        [SerializeField] [Range(3, 65)] private int HEAT_VERTEX_NUM; //顶点细分数
+        private float HEAT_GRID_SIZE; //网格边长
+        private float HEAT_CENTER_DEGREE = 1f;
+        [SerializeField] [Range(0, 0.005f)] private float HEAT_BUOYANCY_FACTOR;
 
-
-        [Header("ParticlePrefab")]
+        [Header("Particle Prefab")]
         [SerializeField] private GameObject prefab;
+
 
         [System.Serializable]
         private class ParticleConfig 
@@ -42,11 +45,12 @@ namespace VortexMethod
             public Vector3 vor;
         }
         
-        
         private List<Particle> vortex_particles;
         private List<Particle> tracer_particles;
-
         private List<Particle> tmp_vortex_particles;
+
+        private List<List<List<float>>> heat_field;
+        private List<List<List<Vector2>>> vortex_field;
 
         private void Awake()
         {
@@ -71,10 +75,125 @@ namespace VortexMethod
         #region Init
         void Init()
         {
-            NUM_VORTEX = vortex_particle_configs.Count;
-
+            InitParameter();
+            InitHeatField();
             InitVortex();
             InitTracer();
+        }
+
+        void InitParameter()
+        {
+            NUM_VORTEX = vortex_particle_configs.Count;
+            HEAT_RADIUS = Mathf.Max(1f, HEAT_RADIUS);
+            HEAT_X_SCALE = Mathf.Min(1f, HEAT_X_SCALE);
+            HEAT_Y_SCALE = Mathf.Min(1f, HEAT_Y_SCALE);
+            HEAT_Z_SCALE = Mathf.Min(1f, HEAT_Z_SCALE);
+
+            if (HEAT_VERTEX_NUM % 2 == 0)
+                HEAT_VERTEX_NUM++;
+            HEAT_VERTEX_NUM = Mathf.Max(3, HEAT_VERTEX_NUM);
+
+            HEAT_GRID_SIZE = HEAT_RADIUS / (HEAT_VERTEX_NUM - 1);
+        }
+
+        void InitHeatField()
+        {
+            int medium = HEAT_VERTEX_NUM / 2;
+
+            // 初始化温度场/浮力场
+            heat_field = new List<List<List<float>>>();
+            
+            for (int i = 0; i < HEAT_VERTEX_NUM; i++)
+            {
+                List<List<float>> heat2d = new List<List<float>>();
+                for(int j = 0; j < HEAT_VERTEX_NUM; j++)
+                {
+                    List<float> heat1d = new List<float>();
+                    for(int k = 0; k < HEAT_VERTEX_NUM; k++)
+                    {
+                        float degree = 0;
+
+                        float distance = Mathf.Sqrt((i - medium) * (i - medium) + (j - medium) * (j - medium) + (k - medium) * (k - medium));
+                        if(distance >= medium)
+                        {
+                            degree = 0;
+                        }
+                        else
+                        {
+                            degree = (1 - distance / medium) * HEAT_CENTER_DEGREE;
+                        }
+                        heat1d.Add(degree);
+                    }
+                    heat2d.Add(heat1d);
+                }
+                heat_field.Add(heat2d);
+            }
+
+
+            // 初始化涡度场
+            // vector.x => x分量 vector.y => z分量
+            vortex_field = new List<List<List<Vector2>>>();
+
+            for (int i = 0; i < HEAT_VERTEX_NUM; i++)
+            {
+                List<List<Vector2>> vortex2d = new List<List<Vector2>>();
+                for (int j = 0; j <HEAT_VERTEX_NUM; j++)
+                {
+                    List<Vector2> vortex1d = new List<Vector2>();
+                    for (int k = 0; k < HEAT_VERTEX_NUM; k++)
+                    {
+                        vortex1d.Add(new Vector2(0f, 0f));
+                    }
+                    vortex2d.Add(vortex1d);
+                }
+                vortex_field.Add(vortex2d);
+            }
+
+            // 有限差分计算涡度场
+            // curl(F) = (dFz/y - dFy/z) x + (dFx/z + dFz/x)y + (dFy/x + dFx/y)z 
+            // 假设浮力向上，则有dFx = dFz = 0, 所以curl(F) = (dFy/z)x + (dFy/x)z
+            for (int i = 0; i < HEAT_VERTEX_NUM; i++)
+            {
+
+                for(int j = 0; j < HEAT_VERTEX_NUM; j++)
+                {
+                    for(int k = 0; k < HEAT_VERTEX_NUM; k++)
+                    {
+                        float curlF_x;
+                        float curlF_z;
+                        if(k == 0)
+                        {
+                            curlF_x = heat_field[i][j][k + 1] - heat_field[i][j][k];
+                        }
+                        else if(k == HEAT_VERTEX_NUM - 1)
+                        {
+                            curlF_x = heat_field[i][j][k] - heat_field[i][j][k - 1];
+                        }
+                        else
+                        {
+                            curlF_x = heat_field[i][j][k + 1] - heat_field[i][j][k - 1];
+                        }
+
+                        if(i == 0)
+                        {
+                            curlF_z = heat_field[i + 1][j][k] - heat_field[i][j][k];
+                        }
+                        else if(i == HEAT_VERTEX_NUM - 1)
+                        {
+                            curlF_z = heat_field[i][j][k] - heat_field[i - 1][j][k];
+                        }
+                        else
+                        {
+                            curlF_z = heat_field[i + 1][j][k] - heat_field[i - 1][j][k];
+                        }
+
+                        vortex_field[i][j][k] = new Vector2(curlF_x, curlF_z);
+
+                    }
+
+                }
+
+            }
         }
 
         void InitVortex()
@@ -110,6 +229,58 @@ namespace VortexMethod
 
         void UpdateVortexParticle()
         {
+            int median = HEAT_VERTEX_NUM / 2;
+
+            //温度场作用
+            for(int i = 0; i < NUM_VORTEX; i++)
+            {
+                Vector3 position = vortex_particles[i].data.pos;
+
+                if (Mathf.Abs(position.x) > HEAT_RADIUS
+                    || Mathf.Abs(position.y) > HEAT_RADIUS
+                    || Mathf.Abs(position.z) > HEAT_RADIUS)
+                    continue;
+
+                int x = Mathf.FloorToInt(position.x / HEAT_GRID_SIZE) + median;
+                int y = Mathf.FloorToInt(position.y / HEAT_GRID_SIZE) + median;
+                int z = Mathf.FloorToInt(position.z / HEAT_GRID_SIZE) + median;
+                x = Mathf.Clamp(x, 0, HEAT_VERTEX_NUM - 2);
+                y = Mathf.Clamp(y, 0, HEAT_VERTEX_NUM - 2);
+                z = Mathf.Clamp(z, 0, HEAT_VERTEX_NUM - 2);
+
+                float left_x = position.x / HEAT_GRID_SIZE + median - x;
+                float right_x = 1 - left_x;
+
+                float left_y = position.y / HEAT_GRID_SIZE + median - y;
+                float right_y = 1 - left_y;
+
+                float left_z = position.z / HEAT_GRID_SIZE + median - z;
+                float right_z = 1 - left_z;
+
+                float increment_x = (
+                     right_x * right_y * right_z * vortex_field[x][y][z].x + 
+                     right_x * right_y * left_z * vortex_field[x][y][z + 1].x +
+                     right_x * left_y * right_z * vortex_field[x][y + 1][z].x + 
+                     right_x * left_y * left_z * vortex_field[x][y + 1][z + 1].x +
+                     left_x * right_y * right_z * vortex_field[x + 1][y][z].x +
+                     left_x * right_y * left_z * vortex_field[x + 1][y][z + 1].x +
+                     left_x * left_y * right_z * vortex_field[x + 1][y + 1][z].x +
+                     left_x * left_y * left_z * vortex_field[x + 1][y + 1][z + 1].x) * HEAT_BUOYANCY_FACTOR;
+
+                float increment_z = (
+                     right_x * right_y * right_z * vortex_field[x][y][z].y +
+                     right_x * right_y * left_z * vortex_field[x][y][z + 1].y +
+                     right_x * left_y * right_z * vortex_field[x][y + 1][z].y +
+                     right_x * left_y * left_z * vortex_field[x][y + 1][z + 1].y +
+                     left_x * right_y * right_z * vortex_field[x + 1][y][z].y +
+                     left_x * right_y * left_z * vortex_field[x + 1][y][z + 1].y +
+                     left_x * left_y * right_z * vortex_field[x + 1][y + 1][z].y +
+                     left_x * left_y * left_z * vortex_field[x + 1][y + 1][z + 1].y) * HEAT_BUOYANCY_FACTOR;
+
+                vortex_particles[i].data.vor += new Vector3(increment_x * Dt, 0f, increment_z * Dt);
+            }
+
+            //涡粒子相互作用
             for (int i = 0; i < NUM_VORTEX; i++)
             {
                 tmp_vortex_particles[i] = vortex_particles[i];
@@ -167,7 +338,7 @@ namespace VortexMethod
             Vector3 v = new Vector3(0, 0, 0);
             for (int j = 0; j < pjs.Count; j++)
             {
-                v += compute_v_from_single_vortex(pi, pjs[j]);
+                v += compute_v_from_single_vortex(pi, pjs[j]);  
             }
             return v;
         }
